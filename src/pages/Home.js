270123d8916b26b2184ab781276e31c7ba2be370
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-10-05 16:52:26 +0100

    add home and details page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CasesToday from '../components/CasesToday';
+import CountriesList from '../components/CountriesList';
+
+const Home = () => (
+  <div>
+
+    <CasesToday />
+    <CountriesList />
+  </div>
+);
+
+export default Home;
