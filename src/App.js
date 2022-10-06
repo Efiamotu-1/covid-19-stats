8901c0a6b1a709a338d8ce71665d3f716c528656
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-10-06 20:26:20 +0100

    add pages and index files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Details from './pages/Details';
+import Home from './pages/Home';
+import { fetchCountries } from './redux/countries/countriesSlice';
+import { fetchflags } from './redux/countries/flagSlice';
+import { fetchTodayStats } from './redux/statistics/statsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTodayStats());
+    dispatch(fetchCountries());
+    dispatch((fetchflags()));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-white">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/covid-info" element={<Details />} />
+      </Routes>
     </div>
   );
 }
