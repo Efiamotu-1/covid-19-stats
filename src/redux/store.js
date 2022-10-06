e64929b5db0606c2e784c5675968e534563699cd
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-10-06 20:23:00 +0100

    add redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import countriesSlice from './countries/countriesSlice';
+import flagSlice from './countries/flagSlice';
+import covidStatsSlice from './statistics/covidStatsSlice';
 import statsReducer from './statistics/statsSlice';
 
 const store = configureStore({
   reducer: {
     stats: statsReducer,
+    stateStats: covidStatsSlice,
+    country: countriesSlice,
+    flags: flagSlice,
   },
 });
 
